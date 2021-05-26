f9164b6995cc8c7f0395d354fd21ca6c7c251a24
Patryk Karbownik <patryk.karbownik98@gmail.com>
2021-05-26 21:02:13 +0200

    added class which contains basic Linda language functions

---
+++ src/LindaCommunication.cpp
@@ -0,0 +1,5 @@
+//
+// Created by patryk on 26.05.2021.
+//
+
+#include "LindaCommunication.h"
---
+++ src/LindaCommunication.h
@@ -0,0 +1,17 @@
+
+#ifndef UXP1A_LINDA_POTOKI_LINDACOMMUNICATION_H
+#define UXP1A_LINDA_POTOKI_LINDACOMMUNICATION_H
+
+
+class LindaCommunication {
+private:
+
+public:
+    LindaCommunication();
+    int input();
+    int read();
+    int output();
+};
+
+
+#endif //UXP1A_LINDA_POTOKI_LINDACOMMUNICATION_H
